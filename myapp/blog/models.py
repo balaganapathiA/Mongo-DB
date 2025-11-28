f1473345efc70f41e4c8ac8846cab90e3f2e2221
balaganapathiA <anandabalan333555777@gmail.com>
2025-11-28 22:25:25 +0530

    make migraton and migrate

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField ()
-    img_url = models.URLField(blank=True)
-    def str_(self):
+    content = models.TextField()
+    img_url = models.URLField(null=True)
+    created_at = models.DateField(auto_now_add=True)
+    def __str__(self):
         return self.title
--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -78,10 +78,18 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',    # localhost
+        'PORT': '3306',         # MySQL default port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
 
 
 # Password validation
